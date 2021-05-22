96c2fcb2a890dd0347ebfc1cffbe9bf7a32e1671
Christofer Castillo <ccastillo993a@gmail.com>
2021-05-22 00:02:09 -0500

    MaintenancePage finalizado

--- src/components/icons/static/ShopIcon.jsx
+++ src/components/icons/static/ShopIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Shop = () => {
+const ShopIcon = () => {
 	return (
 		<div className="container-icon s-mr-2">
 			<svg className="icon-static" width="currentSize" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -10,4 +10,4 @@ const Shop = () => {
 	)
 }
 
-export default Shop;
+export default ShopIcon;
--- src/components/pages/MaintenancePage.jsx
+++ src/components/pages/MaintenancePage.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+
+import GhostButton from '../atoms/GhostButton'
+import SecondaryButton from '../atoms/SecondaryButton'
+import CheckIcon from '../icons/special/CheckIcon'
+import CustomizeIcon from '../icons/static/CustomizeIcon'
+import ShopIcon from '../icons/static/ShopIcon'
 import FooterMenu from '../molecules/FooterMenu'
 import HeaderMenu from '../molecules/HeaderMenu'
 
@@ -7,7 +13,79 @@ export const MaintenancePage = () => {
 		<>
 			<HeaderMenu status="maintenance"/>
 			<main className="main-container maintenancepage">
-				<h1>Pagina Mantenimiento</h1>
+				<section className="section-banner border-section">
+					<img className="banner__hero-image" src="https://images.unsplash.com/photo-1590450175122-945cfdd0b2a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" alt="cubone" />
+					<div className="banner__overlay"></div>
+					<article className="ed-grid article__banner m-grid-2">
+						<div className="banner__content m-cols-1">
+							<h1 className="title-color-dark ">Mantenimiento Básico</h1>
+							<p className="content-color-dark pt-24">Enfocado a personas que cuidan su auto día con día. Aquí podemos encontrar los siguientes servicios:</p>
+							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Neumáticos</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Asientos</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Volante</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Frenos</h4>
+								</div>
+							</div>
+							<div className="container-buttons mt-32">
+								<GhostButton urlTo="/" icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
+								<SecondaryButton urlTo="/" icon={ <ShopIcon /> } title="Añádelo" />
+							</div>
+						</div>
+					</article>
+				</section>
+
+				<section className="section-banner border-section">
+					<img className="banner__hero-image" src="https://images.unsplash.com/photo-1518397727759-189caa3b89a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" alt="cubone" />
+					<div className="banner__overlay"></div>
+					<article className="ed-grid article__banner m-grid-2">
+						<div className="banner__content m-cols-1 m-x-2">
+							<h1 className="title-color-dark ">Mantenimiento Avanzado</h1>
+							<p className="content-color-dark pt-24">Enfocado a personas que buscan un mantenimiento más exhaustivo. Aquí podemos encontrar los siguientes servicios:</p>
+							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Neumáticos</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Asientos</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Volante</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Frenos</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Parabrisas</h4>
+								</div>
+								<div className="services__list s-cross-center s-cols-3">
+									<CheckIcon />
+									<h4 className="content-color-dark">Motor</h4>
+								</div>
+							</div>
+							<div className="container-buttons mt-32">
+								<GhostButton urlTo="/" icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
+								<SecondaryButton urlTo="/" icon={ <ShopIcon /> } title="Añádelo" />
+							</div>
+						</div>
+					</article>
+			</section>
 			</main>
 			<FooterMenu status="maintenance" />
 		</>
