d639873709d1cbc4e2bdf527f1e6456ad8fd9d21
Christofer Castillo <ccastillo993a@gmail.com>
2021-06-20 01:16:19 -0500

    Maintenance - Se añade componente SkeletonMaintenance

--- src/components/pages/MaintenancePage.jsx
+++ src/components/pages/MaintenancePage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
 import { startMaintenanceAdvancedData } from '../../actions/maintenanceAdvanced'
 import { startMaintenanceBasicData } from '../../actions/maintenanceBasic'
 import { useFetchAllMaintenances } from '../../hooks/useFetchAllMaintenances'
-
 import GhostButton from '../atoms/GhostButton'
 import SecondaryButton from '../atoms/SecondaryButton'
 import CheckIcon from '../icons/special/CheckIcon'
@@ -11,6 +11,7 @@ import CustomizeIcon from '../icons/static/CustomizeIcon'
 import ShopIcon from '../icons/static/ShopIcon'
 import FooterMenu from '../molecules/FooterMenu'
 import HeaderMenu from '../molecules/HeaderMenu'
+import SkeletonMaintenance from '../skeletons/SkeletonMaintenance'
 
 export const MaintenancePage = () => {
 
@@ -19,14 +20,13 @@ export const MaintenancePage = () => {
 
 	const dispatch = useDispatch();
 	const { data, loading } = useFetchAllMaintenances();
+	const [arrSkeleton, setArrSkeleton] = useState({
+		arrBasicSheleton: 4,
+		arrAdvancedSkeleton: 6
+	})
 	const [showButtons, setShowButtons] = useState(() => JSON.parse(localStorage.getItem("addCart")))
 	const { arrayDetails: arrBasic, nameMaintenance: nameBasic } = useSelector(state => state.dataMaintenanceBasic);
 	const { arrayDetails: arrAdvanced, nameMaintenance: nameAdvanced } = useSelector(state => state.dataMaintenanceAdvanced);
-
-
-	console.log(data)
-	console.log(loading)
-	console.log(showButtons);
 
 	useEffect(() => {
 
@@ -34,14 +34,18 @@ export const MaintenancePage = () => {
 		if( data.length > 0 ){
 			dispatch(startMaintenanceBasicData(data[0]?.nameMaintenance));
 			dispatch( startMaintenanceAdvancedData(data[1]?.nameMaintenance) )
+			setArrSkeleton({
+				arrBasicSheleton: data[0].arrayDetails.length,
+				arrAdvancedSkeleton: data[1].arrayDetails.length
+			})
 		}
-		console.log(data[0]);
 
 	}, [data, dispatch])
 
 	useEffect(() => {
 
 		if( showButtons === null ) {
+
 			localStorage.setItem("addCart", JSON.stringify({
 				shopBasic: false,
 				shopAdvanced: false
@@ -51,7 +55,8 @@ export const MaintenancePage = () => {
 				shopBasic: false,
 				shopAdvanced: false
 			})
-		}else{
+
+		}else {
 			localStorage.setItem("addCart", JSON.stringify(showButtons))
 		}
 
@@ -61,7 +66,6 @@ export const MaintenancePage = () => {
 	const customButtonBasic = () => {
 
 		if( showButtons.shopBasic === false ) {
-			console.log("CUSTOM DENTRO IF");
 			localStorage.setItem("maintenance", null)
 		}
 
@@ -70,7 +74,6 @@ export const MaintenancePage = () => {
 	const customButtonAdvanced = () => {
 
 		if( showButtons.shopAdvanced === false ) {
-			console.log("CUSTOM DENTRO IF");
 			localStorage.setItem("maintenance", null)
 		}
 
@@ -116,90 +119,84 @@ export const MaintenancePage = () => {
 
 	}
 
-
-
 	return (
 		<>
 			<HeaderMenu status="maintenance"/>
 			{
-				loading ? (
-					<h1 className="title-color">Cargando toda la PAGINA...</h1>
+				( loading === true || arrBasic === undefined ) ? (
+					<SkeletonMaintenance dataArrBasicLength={arrSkeleton.arrBasicSheleton} dataArrAdvancedLength={arrSkeleton.arrAdvancedSkeleton}/>
 				) : (
 					<main className="main-container maintenancepage">
-				<section className="section-banner border-section">
-					<img className="banner__hero-image" src={ imageMaintenanceBasicHome } alt="cubone" />
-					<div className="banner__overlay"></div>
-					<article className="ed-grid article__banner m-grid-2">
-						<div className="banner__content m-cols-1">
-							<h1 className="title-color-dark ">Mantenimiento Básico</h1>
-							<p className="content-color-dark pt-24">Enfocado a personas que cuidan su auto día con día. Aquí podemos encontrar los siguientes servicios:</p>
-							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
-								{
-									arrBasic?.map( basicDetails =>
-										<div key={basicDetails._id} className="services__list s-cross-center s-cols-3">
-											<CheckIcon />
-											<h4 className="content-color-dark">{ basicDetails?.nameDetail }</h4>
-										</div>
-									)
-								}
-							</div>
-							<div className="container-buttons mt-32">
-								{
-									(showButtons?.shopBasic === false ) && (
-										<GhostButton event={ customButtonBasic } urlTo={`/maintenances/${nameBasic}`} icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
-									)
+						<section className="section-banner border-section">
+							<img className="banner__hero-image" src={ imageMaintenanceBasicHome } alt="imgBgMaintenanceBasic" />
+							<div className="banner__overlay"></div>
+							<article className="ed-grid article__banner m-grid-2">
+								<div className="banner__content m-cols-1">
+									<h1 className="title-color-dark ">Mantenimiento Básico</h1>
+									<p className="content-color-dark pt-24">Enfocado a personas que cuidan su auto día con día. Aquí podemos encontrar los siguientes servicios:</p>
+									<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+										{
+											arrBasic?.map( basicDetails =>
+												<div key={basicDetails._id} className="services__list s-cross-center s-cols-3">
+													<CheckIcon />
+													<h4 className="content-color-dark">{ basicDetails?.nameDetail }</h4>
+												</div>
+											)
+										}
+									</div>
+									<div className="container-buttons mt-32">
+										{
+											(showButtons?.shopBasic === false ) && (
+												<GhostButton event={ customButtonBasic } urlTo={`/maintenances/${nameBasic}`} icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
+											)
+										}
+										{
+											(showButtons?.shopBasic === false ) ? (
+												<SecondaryButton event={handleMaintenanceBasicShopAdd} icon={<ShopIcon />} title="Añádelo" />
+											) : (
+												<SecondaryButton event={handleMaintenanceBasicShopRemove} icon={<ShopIcon />} title="Quítalo del carrito" othersClass="button-secondary-danger"/>
+											)
+										}
+									</div>
+								</div>
+							</article>
+						</section>
 
-								}
-								{
-									(showButtons?.shopBasic === false ) ? (
-										<SecondaryButton event={handleMaintenanceBasicShopAdd} icon={<ShopIcon />} title="Añádelo" />
-										) : (
-											<SecondaryButton event={handleMaintenanceBasicShopRemove} icon={<ShopIcon />} title="Quítalo del carrito" othersClass="button-secondary-danger"/>
-										)
-
-								}
-							</div>
-						</div>
-					</article>
-				</section>
-
-				<section className="section-banner border-section">
-					<img className="banner__hero-image" src={ imageMaintenanceAdvancedHome } alt="cubone" />
-					<div className="banner__overlay"></div>
-					<article className="ed-grid article__banner m-grid-2">
-						<div className="banner__content m-cols-1 m-x-2">
-							<h1 className="title-color-dark ">Mantenimiento Avanzado</h1>
-							<p className="content-color-dark pt-24">Enfocado a personas que buscan un mantenimiento más exhaustivo. Aquí podemos encontrar los siguientes servicios:</p>
-							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
-								{
-									arrAdvanced?.map( advancedDetails =>
-										<div key={advancedDetails._id} className="services__list s-cross-center s-cols-3">
-											<CheckIcon />
-											<h4 className="content-color-dark">{ advancedDetails?.nameDetail }</h4>
-										</div>
-									)
-								}
-							</div>
-							<div className="container-buttons mt-32">
-								{
-									(showButtons?.shopAdvanced === false ) && (
-										<GhostButton event={ customButtonAdvanced } urlTo={`/maintenances/${nameAdvanced}`} icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
-									)
-
-								}
-								{
-									(showButtons?.shopAdvanced === false ) ? (
-										<SecondaryButton event={handleMaintenanceAdvancedShopAdd} icon={<ShopIcon />} title="Añádelo" />
-										) : (
-											<SecondaryButton event={handleMaintenanceAdvancedShopRemove} icon={<ShopIcon />} title="Quítalo del carrito" othersClass="button-secondary-danger"/>
-										)
-
-								}
-							</div>
-						</div>
-					</article>
-			</section>
-			</main>
+						<section className="section-banner border-section">
+							<img className="banner__hero-image" src={ imageMaintenanceAdvancedHome } alt="imgBgMaintenanceAdvanced" />
+							<div className="banner__overlay"></div>
+							<article className="ed-grid article__banner m-grid-2">
+								<div className="banner__content m-cols-1 m-x-2">
+									<h1 className="title-color-dark ">Mantenimiento Avanzado</h1>
+									<p className="content-color-dark pt-24">Enfocado a personas que buscan un mantenimiento más exhaustivo. Aquí podemos encontrar los siguientes servicios:</p>
+									<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+										{
+											arrAdvanced?.map( advancedDetails =>
+												<div key={advancedDetails._id} className="services__list s-cross-center s-cols-3">
+													<CheckIcon />
+													<h4 className="content-color-dark">{ advancedDetails?.nameDetail }</h4>
+												</div>
+											)
+										}
+									</div>
+									<div className="container-buttons mt-32">
+										{
+											(showButtons?.shopAdvanced === false ) && (
+												<GhostButton event={ customButtonAdvanced } urlTo={`/maintenances/${nameAdvanced}`} icon={<CustomizeIcon />} othersClass="mr-32" title="Personaliza" />
+											)
+										}
+										{
+											(showButtons?.shopAdvanced === false ) ? (
+												<SecondaryButton event={handleMaintenanceAdvancedShopAdd} icon={<ShopIcon />} title="Añádelo" />
+											) : (
+												<SecondaryButton event={handleMaintenanceAdvancedShopRemove} icon={<ShopIcon />} title="Quítalo del carrito" othersClass="button-secondary-danger"/>
+											)
+										}
+									</div>
+								</div>
+							</article>
+						</section>
+					</main>
 				)
 			}
 			<FooterMenu status="maintenance" />
