fb742ad88b7deda6b3f529b8a8e9ea704d071822
Christofer Castillo <ccastillo993a@gmail.com>
2021-06-25 01:37:18 -0500

    Se corrigió el error del ShopNavIcon

--- src/components/icons/dinamic/ShopNavIcon.jsx
+++ src/components/icons/dinamic/ShopNavIcon.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 const ShopNav = ( {width, status} ) => {
 
-	const arrAddCart = useSelector(() => JSON.parse(localStorage.getItem("cart")));
+	let arrAddCart = useSelector(() => JSON.parse(localStorage.getItem("cart")));
+
+	if( arrAddCart.length === null ) {
+		arrAddCart = [];
+	}
 
 	return (
 		<>
