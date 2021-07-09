d3ff1efde9d75b8aa6f03b5007a56f8e0344a03f
Christofer Castillo <ccastillo993a@gmail.com>
2021-07-09 01:44:38 -0500

    Se agrega la carga diferida a cada ruta

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 // Importamos todo lo necesario para usar las rutas
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -7,69 +7,72 @@ import {
 	Redirect
 } from 'react-router-dom';
 
-// Importamos nuestros componentes que se visualizarán para cada ruta
-import { DiscountDetails } from '../components/pages/DiscountDetails';
-import { DiscountPage } from '../components/pages/DiscountPage';
-import { EventPage } from '../components/pages/EventPage';
-import { HomePage } from '../components/pages/HomePage';
-import { MaintenanceDetails } from '../components/pages/MaintenanceDetails';
-import { MaintenancePage } from '../components/pages/MaintenancePage';
-import { ModelDetails } from '../components/pages/ModelDetails';
-import { ModelPage } from '../components/pages/ModelPage';
-import { Shop } from '../components/pages/Shop';
+import { LoadingPage } from '../components/pages/LoadingPage';
+
+const ModelPage = lazy(() => import('../components/pages/ModelPage'));
+const HomePage = lazy(() => import('../components/pages/HomePage'));
+const EventPage = lazy(() => import('../components/pages/EventPage'));
+const MaintenancePage = lazy(() => import('../components/pages/MaintenancePage'));
+const ModelDetails = lazy(() => import('../components/pages/ModelDetails'));
+const MaintenanceDetails = lazy(() => import('../components/pages/MaintenanceDetails'));
+const Shop = lazy(() => import('../components/pages/Shop'));
+const DiscountPage = lazy(() => import('../components/pages/DiscountPage'));
+const DiscountDetails = lazy(() => import('../components/pages/DiscountDetails'));
 
 // Exportamos cada una de nuestras rutas con su respectivo componente
 export const AppRouter = () => {
 	return (
 		<Router>
-			<Switch>
-				<Route
-					exact
-					path="/shop"
-					component={ Shop }
-				/>
-				<Route
-					exact
-					path="/events"
-					component={ EventPage }
-				/>
-				<Route
-					exact
-					path="/discounts/:nameDiscount"
-					component={ DiscountDetails }
-				/>
-				<Route
-					exact
-					path="/discounts"
-					component={ DiscountPage }
-				/>
-				<Route
-					exact
-					path="/maintenances/:nameType"
-					component={ MaintenanceDetails }
-				/>
-				<Route
-					exact
-					path="/maintenances"
-					component={ MaintenancePage }
-				/>
-				<Route
-					exact
-					path="/models/:nameModel"
-					component={ ModelDetails }
-				/>
-				<Route
-					exact
-					path="/models"
-					component={ ModelPage }
-				/>
-				<Route
-					exact
-					path="/"
-					component={ HomePage }
-				/>
-				<Redirect to="/" />
-			</Switch>
+			<Suspense fallback={<LoadingPage />}>
+				<Switch>
+					<Route
+						exact
+						path="/shop"
+						component={ Shop }
+					/>
+					<Route
+						exact
+						path="/events"
+						component={ EventPage }
+					/>
+					<Route
+						exact
+						path="/discounts/:nameDiscount"
+						component={ DiscountDetails }
+					/>
+					<Route
+						exact
+						path="/discounts"
+						component={ DiscountPage }
+					/>
+					<Route
+						exact
+						path="/maintenances/:nameType"
+						component={ MaintenanceDetails }
+					/>
+					<Route
+						exact
+						path="/maintenances"
+						component={ MaintenancePage }
+					/>
+					<Route
+						exact
+						path="/models/:nameModel"
+						component={ ModelDetails }
+					/>
+					<Route
+						exact
+						path="/models"
+						component={ ModelPage }
+					/>
+					<Route
+						exact
+						path="/"
+						component={ HomePage }
+					/>
+					<Redirect to="/" />
+				</Switch>
+			</Suspense>
 		</Router>
 	)
 }
