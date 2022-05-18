0befdc510a77de5df3e42ced51dd3663ee99e977
Christofer Castillo <ccastillo993a@gmail.com>
2022-05-18 17:22:52 -0500

    Se corrige el bug de color en los Skeletons

--- src/components/skeletons/SkeletonMaintenance.jsx
+++ src/components/skeletons/SkeletonMaintenance.jsx
@@ -1,83 +1,111 @@
-
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 import SkeletonHeader from './SkeletonHeader';
 
 const SkeletonMaintenance = ({ dataArrBasicLength, dataArrAdvancedLength }) => {
+  return (
+    <>
+      <SkeletonHeader />
+      <SkeletonTheme baseColor="#8e8e8f">
+        <main className="main-container maintenancepage">
+          <section className="section-banner border-section">
+            <div height={`100%`} width={`100%`} />
+            <article className="ed-grid article__banner m-grid-2">
+              <div className="banner__content m-cols-1">
+                <h1 className="title-color-dark ">
+                  <Skeleton width={`70%`} />
+                </h1>
+                <p className="content-color-dark pt-24">
+                  <Skeleton width={`100%`} />
+                  <Skeleton width={`85%`} />
+                </p>
+                <div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+                  {Array(dataArrBasicLength)
+                    .fill()
+                    .map((basicDetails, index) => (
+                      <div
+                        key={index}
+                        className="services__list s-cross-center s-cols-3"
+                      >
+                        <Skeleton
+                          className="container-icon s-mr-2"
+                          width={10}
+                        />
+                        <h4 className="content-color-dark">
+                          <Skeleton width={80} />
+                        </h4>
+                      </div>
+                    ))}
+                </div>
+                <div className="container-buttons mt-32">
+                  <Skeleton
+                    className="button button-ghost mr-32"
+                    width={100}
+                    height={30}
+                  />
+                  <Skeleton
+                    className="button button-secondary"
+                    width={100}
+                    height={30}
+                  />
+                </div>
+              </div>
+            </article>
+          </section>
 
-	return(
-
-		<>
-		<SkeletonHeader />
-		<SkeletonTheme color="#8e8e8f" >
-			<main className="main-container maintenancepage">
-				<section className="section-banner border-section">
-					<div className="skeleton_bg_maintenance" height={`100%`} width={`100%`} />
-					<article className="ed-grid article__banner m-grid-2">
-						<div className="banner__content m-cols-1">
-							<h1 className="title-color-dark ">
-								<Skeleton width={`70%`} />
-							</h1>
-							<p className="content-color-dark pt-24">
-								<Skeleton width={`100%`} />
-								<Skeleton width={`85%`} />
-							</p>
-							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
-								{
-									Array(dataArrBasicLength).fill().map( (basicDetails, index) =>
-										<div key={index} className="services__list s-cross-center s-cols-3">
-											<Skeleton className="container-icon s-mr-2" width={10} />
-											<h4 className="content-color-dark">
-												<Skeleton width={80} />
-											</h4>
-										</div>
-									)
-								}
-							</div>
-							<div className="container-buttons mt-32">
-								<Skeleton className="button button-ghost mr-32" width={100} height={30} />
-								<Skeleton className="button button-secondary" width={100} height={30} />
-							</div>
-						</div>
-					</article>
-				</section>
-
-				<section className="section-banner border-section">
-					<div className="skeleton_bg_maintenance" height={`100%`} width={`100%`} />
-					<article className="ed-grid article__banner m-grid-2">
-						<div className="banner__content m-cols-1 m-x-2">
-							<h1 className="title-color-dark ">
-								<Skeleton width={`85%`} />
-							</h1>
-							<p className="content-color-dark pt-24">
-								<Skeleton width={`100%`} />
-								<Skeleton width={`90%`} />
-							</p>
-							<div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
-								{
-									Array(dataArrAdvancedLength).fill().map( (advancedDetails, index) =>
-										<div key={index} className="services__list s-cross-center s-cols-3">
-											<Skeleton className="container-icon s-mr-2" width={10} />
-											<h4 className="content-color-dark">
-												<Skeleton width={80} />
-											</h4>
-										</div>
-									)
-								}
-							</div>
-							<div className="container-buttons mt-32">
-								<Skeleton className="button button-ghost mr-32" width={100} height={30} />
-								<Skeleton className="button button-secondary" width={100} height={30} />
-							</div>
-						</div>
-					</article>
-				</section>
-			</main>
-		</SkeletonTheme>
-		</>
-
-	)
-}
+          <section className="section-banner border-section">
+            <div
+              className="skeleton_bg_maintenance"
+              height={`100%`}
+              width={`100%`}
+            />
+            <article className="ed-grid article__banner m-grid-2">
+              <div className="banner__content m-cols-1 m-x-2">
+                <h1 className="title-color-dark ">
+                  <Skeleton width={`85%`} />
+                </h1>
+                <p className="content-color-dark pt-24">
+                  <Skeleton width={`100%`} />
+                  <Skeleton width={`90%`} />
+                </p>
+                <div className="container-services pt-20 ed-grid col-gap-32 s-grid-6 row-gap-4">
+                  {Array(dataArrAdvancedLength)
+                    .fill()
+                    .map((advancedDetails, index) => (
+                      <div
+                        key={index}
+                        className="services__list s-cross-center s-cols-3"
+                      >
+                        <Skeleton
+                          className="container-icon s-mr-2"
+                          width={10}
+                        />
+                        <h4 className="content-color-dark">
+                          <Skeleton width={80} />
+                        </h4>
+                      </div>
+                    ))}
+                </div>
+                <div className="container-buttons mt-32">
+                  <Skeleton
+                    className="button button-ghost mr-32"
+                    width={100}
+                    height={30}
+                  />
+                  <Skeleton
+                    className="button button-secondary"
+                    width={100}
+                    height={30}
+                  />
+                </div>
+              </div>
+            </article>
+          </section>
+        </main>
+      </SkeletonTheme>
+    </>
+  );
+};
 
 export default SkeletonMaintenance;
--- src/components/skeletons/SkeletonModelCard.jsx
+++ src/components/skeletons/SkeletonModelCard.jsx
@@ -1,46 +1,53 @@
-
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 import SkeletonHeader from './SkeletonHeader';
 
 const SkeletonModelCard = ({ dataArrLength }) => {
-
-	return(
-
-		<>
-		<SkeletonHeader />
-		<SkeletonTheme color="#8e8e8f" >
-		<main className="main-container modelpage">
-			<section className="section-card ed-grid s-grid-12 rows-gap">
-				{
-					Array(dataArrLength).fill().map( (card, index) => (
-						<div key={index} className="p-relative card-container s-cols-12 m-cols-4 lg-cols-3">
-							<div className="container__img s-ratio-16-9 img-container">
-								<Skeleton style={{ position: "absolute" }} height={`100%`} width={`100%`} />
-							</div>
-							<div className="container__text s-pxy-4">
-								<h3 className="title-color">
-									<Skeleton width={`60%`} />
-								</h3>
-								<h4 className="text__description content-color s-pt-2">
-									<Skeleton width={`100%`} />
-									<Skeleton width={`95%`} />
-								</h4>
-								<h4 className="content-color s-pt-4">
-									<Skeleton width={`20%`} />
-								</h4>
-								<Skeleton className="mt-32 button button-secondary" width={`40%`} height={30} />
-							</div>
-						</div>
-					))
-				}
-			</section>
-		</main>
-		</SkeletonTheme>
-		</>
-
-	)
-}
+  return (
+    <>
+      <SkeletonHeader />
+      <SkeletonTheme baseColor="#8e8e8f">
+        <main className="main-container modelpage">
+          <section className="section-card ed-grid s-grid-12 rows-gap">
+            {Array(dataArrLength)
+              .fill()
+              .map((card, index) => (
+                <div
+                  key={index}
+                  className="p-relative card-container s-cols-12 m-cols-4 lg-cols-3"
+                >
+                  <div className="container__img s-ratio-16-9 img-container">
+                    <Skeleton
+                      style={{ position: 'absolute' }}
+                      height={`100%`}
+                      width={`100%`}
+                    />
+                  </div>
+                  <div className="container__text s-pxy-4">
+                    <h3 className="title-color">
+                      <Skeleton width={`60%`} />
+                    </h3>
+                    <h4 className="text__description content-color s-pt-2">
+                      <Skeleton width={`100%`} />
+                      <Skeleton width={`95%`} />
+                    </h4>
+                    <h4 className="content-color s-pt-4">
+                      <Skeleton width={`20%`} />
+                    </h4>
+                    <Skeleton
+                      className="mt-32 button button-secondary"
+                      width={`40%`}
+                      height={30}
+                    />
+                  </div>
+                </div>
+              ))}
+          </section>
+        </main>
+      </SkeletonTheme>
+    </>
+  );
+};
 
 export default SkeletonModelCard;
