daf173e235ad08abc2f58ef15b2914d545e9ea5a
Christofer Castillo <ccastillo993a@gmail.com>
2022-05-18 16:42:19 -0500

    Creación de nuevos types

--- src/types/types.js
+++ src/types/types.js
@@ -1,13 +1,24 @@
-
 export const types = {
+  modelData: '[model] ModelData loaded',
+  modelResetData: '[model] ModelData reset',
 
-	modelData: '[model] ModelData loaded',
-	modelResetData: '[model] ModelData reset',
+  maintenanceBasicData: '[maintenance] BasicData loaded',
+  maintenanceAdvancedData: '[maintenance] AdvancedData loaded',
+  uploadCustomService: '[maintenance] Upload custom services',
+  updateCustomServices: '[maintenance] Update custom services',
+  resetCustomService: '[maintenance] Reset custom services',
 
-	maintenanceBasicData: '[maintenance] BasicData loaded',
-	maintenanceAdvancedData: '[maintenance] AdvancedData loaded',
+  discountData: '[discount] DiscountData loaded',
+  discountResetData: '[discount] DiscountData reset',
 
-	discountData: '[discount] DiscountData loaded',
-	discountResetData: '[discount] DiscountData reset',
+  initializateCart: '[cart] Initializate products in cart',
+  addToCart: '[cart] Add product to cart',
+  removeToCart: '[cart] Remove product to cart',
 
-}
+  uploadProduct: '[product] Upload current product',
+  updateProduct: '[product] Update current product',
+  resetProduct: '[product] Reset current product',
+
+  uiShowModelImage: '[ui] Show model image',
+  uiHiddenModelImage: '[ui] Hidden model image',
+};
