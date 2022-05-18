f5d8a98cb72d2ba6e99394132447d843ab51a61b
Christofer Castillo <ccastillo993a@gmail.com>
2022-05-18 17:08:09 -0500

    Se elimina la importación del paquete react-toastify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
-
 import React from 'react';
-import 'react-toastify/dist/ReactToastify.css';
 
-import "../scss/styles.scss";
-import { AppRouter } from '../routers/AppRouter';
+import '../scss/styles.scss';
+import AppRouter from '../routers/AppRouter';
 import { configureStore } from './store/store';
 import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-		<Provider store={ configureStore() }>
-			<AppRouter />
-		</Provider>
-  )
-}
+    <Provider store={configureStore()}>
+      <AppRouter />
+    </Provider>
+  );
+};
 
 export default App;
