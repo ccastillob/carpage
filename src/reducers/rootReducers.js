5debed07baefea6a794f88f7b0bef0205f055d60
Christofer Castillo <ccastillo993a@gmail.com>
2022-05-18 16:43:27 -0500

    Se añade nuevos reducers a rootReducers

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,14 +1,21 @@
+import { combineReducers } from 'redux';
 
-import { combineReducers } from 'redux'
-
-import { discountReducer } from './discountReducer'
-import { maintenanceAdvancedReducer } from './maintenanceAdvancedReducer'
-import { maintenanceBasicReducer } from './maintenanceBasicReducer'
-import { modelReducer } from './modelReducer'
+import { discountReducer } from './discountReducer';
+import { maintenanceAdvancedReducer } from './maintenanceAdvancedReducer';
+import { maintenanceBasicReducer } from './maintenanceBasicReducer';
+import { maintenanceCustomServiceReducer } from './maintenanceReducer';
+import { modelReducer } from './modelReducer';
+import { productReducer } from './productReducer';
+import { shoppingCartReducer } from './shoppingCartReducer';
+import { uiReducer } from './uiReducer';
 
 export const rootReducers = combineReducers({
-	dataModel: modelReducer,
-	dataMaintenanceBasic: maintenanceBasicReducer,
-	dataMaintenanceAdvanced: maintenanceAdvancedReducer,
-	dataDiscount: discountReducer,
-})
+  dataModel: modelReducer,
+  dataMaintenanceBasic: maintenanceBasicReducer,
+  dataMaintenanceAdvanced: maintenanceAdvancedReducer,
+  dataDiscount: discountReducer,
+  shoppingCart: shoppingCartReducer,
+  currentMaintenanceServices: maintenanceCustomServiceReducer,
+  currentProduct: productReducer,
+  ui: uiReducer,
+});
